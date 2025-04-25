2ca72df936bd247782bf95dd95fab1b8b627e644
pitang.hilderney <hilderney.zapf@pitang.com>
2025-04-25 19:12:33 -0300

    project init

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideIonicAngular({}), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
+};
