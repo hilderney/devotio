7cc1350f9fe633683ed00c32bc193f252108516b
pitang.hilderney <hilderney.zapf@pitang.com>
2025-04-28 22:18:14 -0300

    devotional-itens com evento de swipe

--- src/app/pages/devotional/devotional.component.ts
+++ src/app/pages/devotional/devotional.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { PageLayoutComponent } from '../../components/page-layout.component';
+import { PageLayoutComponent } from '../../components/page-layout/page-layout.component';
+import { DevotionalItemComponent } from '../../components/devotional-item/devotional-item.component';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-devotional',
   templateUrl: './devotional.component.html',
   styleUrls: ['./devotional.component.scss'],
   standalone: true,
-  imports: [PageLayoutComponent]
+  imports: [
+    CommonModule,
+    IonicModule,
+    PageLayoutComponent,
+    DevotionalItemComponent]
 })
 export class DevotionalComponent  implements OnInit {
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { addIcons } from 'ionicons';
+import { starOutline, arrowBackOutline, checkmarkOutline, closeOutline } from 'ionicons/icons';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+addIcons({
+  'star-outline': starOutline,
+  'arrow-back-outline': arrowBackOutline,
+  'checkmark-outline': checkmarkOutline,
+  'close-outline': closeOutline
+});
+
+// Carrega os elementos personalizados do Ionic
+defineCustomElements(window);
