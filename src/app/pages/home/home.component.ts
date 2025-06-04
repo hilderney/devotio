1d2101e17626b715eecc62dfcef2be949c17bd63
pitang.hilderney <hilderney.zapf@pitang.com>
2025-06-04 14:41:02 -0300

    bugfix build prod scropt optimization

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { MainCardComponent } from '../../shared/components/main-card/main-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { MainCardComponent } from '../../shared/components/main-card/main-card.c
   styleUrls: ['./home.component.scss'],
   standalone: true,
   imports: [
+    CommonModule,
     IonicModule,
     RouterModule,
     MainCardComponent
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { PageLayoutComponent } from '../../shared/components/page-layout/page-layout.component';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   standalone: true,
-  imports: [PageLayoutComponent]
+  imports: [
+    PageLayoutComponent,
+    CommonModule,
+    IonicModule
+  ]
 })
 export class ProfileComponent  implements OnInit {
 
